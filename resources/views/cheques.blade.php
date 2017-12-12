a95990da0625e9047c54c56dcdf8484918ba4835
naby <abaodum@gmail.com>
2017-12-12 16:08:22 +0000

    first commit

--- resources/views/cheques.blade.php
+++ resources/views/cheques.blade.php
@@ -261,7 +261,7 @@
 
                 $('#banks').append($('<option>', {
                     value: item.bank_name,
-                    text: item.bank_name
+                    text: item.bank_name +'- '+item.account_type+' - '+item.account_no
                 }));
             });
 
